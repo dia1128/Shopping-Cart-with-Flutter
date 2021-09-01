16e78d85bed1ab8fd1737484645baf351c97a36d
dia1128 <safrin1128@gmail.com>
2021-09-01 14:00:38 -0500

    Added Chart and Chart Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,130 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:shopping_app/widgets/new_transaction.dart';
-import 'package:shopping_app/widgets/transaction_list.dart';
-import '../models/transaction.dart';
 
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
+import './widgets/chart.dart';
+import './models/transaction.dart';
 
-
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Flutter APP",
+      title: 'Personal Expenses',
+      theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.amber,
+          fontFamily: 'Quicksand',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+          appBarTheme: AppBarTheme(
+            textTheme: ThemeData.light().textTheme.copyWith(
+                  title: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+          )),
       home: MyHomePage(),
     );
   }
 }
 
-
-
 class MyHomePage extends StatefulWidget {
+  // String titleInput;
+  // String amountInput;
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
-  final List <Transaction> _userTransaction = [
-    Transaction("t1", "Grocery", 69.99, DateTime.now()),
-    Transaction("t2", "Shoes",80.99, DateTime.now()),
+  final List<Transaction> _userTransactions = [
+    // Transaction(
+    //   id: 't1',
+    //   title: 'New Shoes',
+    //   amount: 69.99,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Weekly Groceries',
+    //   amount: 16.53,
+    //   date: DateTime.now(),
+    // ),
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
 
-
-  void _addNewTransaction(String txTitle, double txAmount){
+  void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
-        DateTime.now().toString(),
-        txTitle,
-        txAmount,
-        DateTime.now()
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
     );
 
     setState(() {
-      _userTransaction.add(newTx);
+      _userTransactions.add(newTx);
     });
   }
 
-
-  void _startAddNewTransaction(BuildContext ctx)
-  {
+  void _startAddNewTransaction(BuildContext ctx) {
     showModalBottomSheet(
       context: ctx,
       builder: (_) {
-        return GestureDetector (
-            onTap: () {},
-            child: NewTransaction(_addNewTransaction),
-            behavior: HitTestBehavior.opaque,
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
         );
-
-    },);
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
+        title: Text(
+          'Personal Expenses',
+        ),
         actions: <Widget>[
           IconButton(
-              icon: Icon(Icons.add), onPressed: () =>_startAddNewTransaction(context),), //Button on the top of the page
-        ]
-
-      ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-            Container(
-
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,),
-            ),
-
-
-          TransactionList(_userTransaction),
-
-          ],
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
           ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Chart(_recentTransactions),
+            TransactionList(_userTransactions),
+          ],
         ),
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => _startAddNewTransaction(context),
-
       ),
-
     );
-
   }
 }
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,77 +1,67 @@
 import 'package:flutter/material.dart';
 
+class NewTransaction extends StatefulWidget {
+  final Function addTx;
 
-class  NewTransaction extends StatefulWidget {
-
- final Function addTx;
-
-NewTransaction(this.addTx);
+  NewTransaction(this.addTx);
 
   @override
   _NewTransactionState createState() => _NewTransactionState();
-}  
+}
 
 class _NewTransactionState extends State<NewTransaction> {
- final titleController = TextEditingController();
+  final titleController = TextEditingController();
 
- final amountController = TextEditingController();
+  final amountController = TextEditingController();
 
-void submitData ()
-{
-  final enteredTitle = titleController.text;
-  final enteredAmount = double.parse(amountController.text);
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
 
-  if(enteredTitle.isEmpty || enteredAmount <= 0)
-    {
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
       return;
     }
-  widget.addTx(
-     enteredTitle,
-     enteredAmount,
-  );
-}
+
+    widget.addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-    elevation: 5,
-    child: Container(
-    padding: EdgeInsets.all(10),
-    child: Column(
-    crossAxisAlignment: CrossAxisAlignment.end,
-    children: <Widget>[
-    TextField(
-    autocorrect: true,
-    autofocus: true,
-    decoration: InputDecoration(labelText: 'Title',),
-    // onChanged: (val) {
-    //   TextEditingController() = val;
-    // },
-      controller : titleController,
-      onSubmitted: (_) => submitData,
-    ) ,
-    TextField(autocorrect: true,
-    autofocus: true,
-    decoration: InputDecoration(labelText: 'Amount'),
-
-
-      controller: amountController,
-keyboardType: TextInputType.number,
-onSubmitted: (_) => submitData, //anonymous function, underscore doesn't mean antything
-// onChanged: (val){
-// amountInput = double.parse(val);
-// },
-
-),
-FlatButton(
-
-  onPressed:  submitData,
-     child: Text('Add Transaction'),
-     textColor: Colors.purple,
-)
-],
-    ),
-)
-);
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+              onSubmitted: (_) => submitData(),
+              // onChanged: (val) {
+              //   titleInput = val;
+              // },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
+              // onChanged: (val) => amountInput = val,
+            ),
+            FlatButton(
+              child: Text('Add Transaction'),
+              textColor: Colors.purple,
+              onPressed: submitData,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
